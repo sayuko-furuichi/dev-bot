9138b10ff1c182b150cc2fb01f2cf282f809d8d7
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-22 11:47:36 +0900

    aaa

--- app/Http/Controllers/LinkId.php
+++ app/Http/Controllers/LinkId.php
@@ -8,10 +8,31 @@ class LinkId extends Controller
 {
     public function index()
     {
+
+        $channelSecret = '0b0aadd7b81ec25d7d861c28846e4048'; // Channel secret string
+        $httpRequestBody = '...'; // Request body string
+        $hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
+        $signature = base64_encode($hash);
+
+
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=');
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0b0aadd7b81ec25d7d861c28846e4048']);
 
         $response = $bot->replyText('<reply token>', 'hello!');
         return $response ;
+
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage('<reply token>', $textMessageBuilder);
+if ($response->isSucceeded()) {
+    echo 'Succeeded!';
+    return;
+}
+
+// Failed
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+
+
+
     }
 }
