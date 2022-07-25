90b4c9848421625d0bc86f5d2e3bb4b14f031323
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-25 14:17:26 +0900

    use

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot;
 
 class Callback extends Controller
 {
