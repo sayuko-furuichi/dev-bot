c87830cf000584bc3252892c51d9203d9ba45b48
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-26 11:54:24 +0900

    ss

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use LINE\LINEBot;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +18,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-$httpClient = new CurlHTTPClient($_ENV['LINE_CHANNEL_ACCESS_TOKEN']);
-$bot = new LINEBot($httpClient, ['channelSecret' => $_ENV['LINE_CHANNEL_SECRET']]);
-
-Route::post('/', function (Request $request) use ($bot) {
-    $request->collect('events')->each(function ($event) use ($bot) {
-        $bot->replyText($event['replyToken'], $event['message']['text']);
-    });
-    return 'ok!';
-});
