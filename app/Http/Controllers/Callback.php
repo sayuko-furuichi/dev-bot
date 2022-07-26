d363ad8e46b5ea4446c107919a9adf1e8952035c
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-26 12:23:17 +0900

    aa

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -12,6 +12,10 @@ class Callback extends Controller
     //
     public function index(Request $request){
 
+
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0b0aadd7b81ec25d7d861c28846e4048']);
+
         $bot = app('line-bot');
         // LINE シグネチャのチェック
         $signature = $_SERVER['HTTP_' . LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
