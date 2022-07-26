2a99354f8e73d3dfa8a3f21bbecd0f280bcb58f5
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-26 13:45:31 +0900

    ss

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -16,12 +16,6 @@ class Callback extends Controller
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0b0aadd7b81ec25d7d861c28846e4048']);
         $at= '/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=';
 
-  //  var_dump($request);
-        $dData =json_decode($request, true);
-
-        var_dump($dData);
-
-        $reply_token = $dData["replyToken"];
     
         // ここに自動応答処理を書く
         
@@ -33,25 +27,6 @@ class Callback extends Controller
 
         echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
      
-        //  return $response;
-        $body=['replyToken'=>'','message'=>['type'=>'text','text'=>'hello']];
-
-        $jenco=json_encode($body,true);
-
-        $api_url ='https://api.line.me/v2/bot/message/reply';
-
-        //エンコードされたURLで通信する
-        $headers = [ "application/json","Authorization:Bearer $at"];
-    
-        $curl_handle = curl_init();
-    
-        curl_setopt($curl_handle, CURLOPT_POST, true);
-        curl_setopt($curl_handle, CURLOPT_URL, $api_url);
-        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, "$jenco");
-        curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
-        // curl_exec()の結果を文字列にする
-        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
-
-        curl_close($curl_handle);
+   
     }
 }
