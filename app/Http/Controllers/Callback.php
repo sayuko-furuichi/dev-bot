baad35d7e9271df219d952f6daf6456fb3280a1a
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-26 17:49:14 +0900

    ss

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot;
