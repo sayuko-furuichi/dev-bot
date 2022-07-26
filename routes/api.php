39160f3a564e3926abb66ab552c227d0c2b22d9d
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-26 17:12:08 +0900

    ss

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['namespace' => 'api'], function () {
+
     Route::get('/callback', 'App\Http\Controllers\Callback@text')->name('line.callback');
-});
+
 
