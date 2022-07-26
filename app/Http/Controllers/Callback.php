91956e7ccf463ea7408d267de646460f4cf063cc
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-26 13:15:41 +0900

    ああ

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -10,31 +10,28 @@ use LINE\LINEBot;
 class Callback extends Controller
 {
     //
-    public function index(Request $request){
-
-
+    public function index(Request $request)
+    {
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0b0aadd7b81ec25d7d861c28846e4048']);
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0b0aadd7b81ec25d7d861c28846e4048']);
 
     
-       $dData =json_decode($request,true);
+        $dData =json_decode($request, true);
 
-if (isset($dData["events"]) ){
-}
+        var_dump($dData);
+
         $reply_token = $dData["replyToken"];
     
         // ここに自動応答処理を書く
         
-        $response=$bot->replyText($reply_token, 'Hello');
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+        
+        $response=$bot->replyText($reply_token, $textMessageBuilder);
     
         // JSONでステータスコード=200のレスポンスを返す
-        response()->json(['return-data' => 'data'], 200);
+
+        echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
      
-        return $response;
-    
+        //  return $response;
     }
-
-
-
-
 }
