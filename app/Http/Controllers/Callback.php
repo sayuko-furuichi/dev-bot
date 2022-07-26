0d616cd15262764f5dca317df537224250bf0239
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-26 13:24:56 +0900

    ｓｓ

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -15,7 +15,7 @@ class Callback extends Controller
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=');
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0b0aadd7b81ec25d7d861c28846e4048']);
 
-    
+    var_dump($request);
         $dData =json_decode($request, true);
 
         var_dump($dData);
