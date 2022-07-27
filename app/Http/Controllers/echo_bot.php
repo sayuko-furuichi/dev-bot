b30910e93a930d460fca7a3c32bf4de4aa1f14f5
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-27 09:54:00 +0900

    ss

--- app/Http/Controllers/echo_bot.php
+++ app/Http/Controllers/echo_bot.php
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-require_once('./LINEBotTiny.php');
+require_once('App\Http\Controllers\LINEBotTiny.php');
 
 $channelAccessToken = '/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '0b0aadd7b81ec25d7d861c28846e4048';
