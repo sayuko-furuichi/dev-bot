c893f21c97fa2a11533d226354af1d8f763e7dc8
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-27 15:09:36 +0900

    いけるか？

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -64,7 +64,7 @@ class Callback extends Controller
                 ],
                 [
                     'type' => 'text',
-                    'text' => $event['source']
+                    'text' => $message['source']
                 ]
             ]
         ]);
