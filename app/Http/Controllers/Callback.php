2f7e8eed90c0a5fb72c677e503de098e483230f7
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-27 10:12:32 +0900

    ss

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -143,7 +143,7 @@ $signature = $_SERVER['HTTP_'.LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
     public function tiny(){
 
     
-require_once('\LINEBotTiny.php');
+require_once('..\LINEBotTiny.php');
 
 $channelAccessToken = '/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '0b0aadd7b81ec25d7d861c28846e4048';
