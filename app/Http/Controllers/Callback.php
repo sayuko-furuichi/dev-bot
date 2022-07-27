581f5b4eaa647953a6e283a5dc1aedffbf37e21e
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-27 16:06:15 +0900

    s

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -69,7 +69,7 @@ class Callback extends Controller
                 ],
                 [
                     'type' => 'text',
-                    'text' =>  $id.$userId.$usenm
+                    'text' =>  $id
                 ]
             ]
         ]);
