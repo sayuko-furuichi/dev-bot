628548310e05ef7ae54083814abad136ce428030
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-01 14:12:08 +0900

    aa

--- app/Http/Controllers/SendMessage.php
+++ app/Http/Controllers/SendMessage.php
@@ -64,7 +64,7 @@ class SendMessage extends Controller
 
                     $dcd=json_decode($res,true);
 
-                    if($dcd==false || $dcd== null ||$dcd== 'undefine' || !isset($dcd[0]) ){
+                    if($dcd==false || $dcd== null ||$dcd== 'undefine' || !isset($dcd['richMenuId']) ){
                         $flag='false';
                     }else{
                         $flag='true';
