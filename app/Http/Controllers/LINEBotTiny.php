de5f1c1a053fab8c7a1e4ef0908d7ad2c6536856
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-02 15:28:56 +0900

    cd

--- app/Http/Controllers/LINEBotTiny.php
+++ app/Http/Controllers/LINEBotTiny.php
@@ -215,7 +215,7 @@ class LINEBotTiny
             ],
         ]);
 
-        $response = file_get_contents('https://api-data.line.me/v2/bot/richmenu/'. $richmenuId . '/content', false, $context);
+        $response = file_get_contents('https://api-data.line.me/v2/bot/richmenu/richmenu-b56771c2cf5b359b8c182d7de6f9e2c8/content', false, $context);
         if (strpos($http_response_header[0], '200') === false) {
             error_log('Request failed: ' . $response);
         }
