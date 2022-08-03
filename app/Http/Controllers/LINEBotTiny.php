3156cb15aba05565069a018e14c9cbfebd2c1e0f
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-03 14:56:21 +0900

    aa

--- app/Http/Controllers/LINEBotTiny.php
+++ app/Http/Controllers/LINEBotTiny.php
@@ -280,11 +280,11 @@ class LINEBotTiny
         ]);
 
         $dfresponse = file_get_contents('https://api.line.me/v2/bot/user/all/richmenu/richmenu-3314b59741163f1ee95266baeb8e955f', false, $dfcontext);
-        if (strpos($http_response_header[0], '200') === false) {
-            $dfresponse= 'Request failed';
-          }else{
-            $dfresponse= 'OK';
-           }
+        // if (strpos($http_response_header[0], '200') === false) {
+        //     $dfresponse= 'Request failed';
+        //   }else{
+        //     $dfresponse= 'OK';
+        //    }
 
         return $dfresponse;
 
