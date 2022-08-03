65182716db5b9da2d9e8b8c432eb6b345b8e2c39
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-03 11:44:07 +0900

    ss

--- app/Http/Controllers/LINEBotTiny.php
+++ app/Http/Controllers/LINEBotTiny.php
@@ -214,7 +214,7 @@ class LINEBotTiny
                 'ignore_errors' => true,
                 'method' => 'POST',
                 'header' => implode("\r\n", $imgheader),
-               'content' => urlencode($imgurl)
+               'content' => implode("\r\n",urlencode($imgurl))
             ],
         ]);
 
