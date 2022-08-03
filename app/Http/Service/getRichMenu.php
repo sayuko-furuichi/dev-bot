c1bc4496528adb7edb85449f664b2a5be05d47fb
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-03 16:07:36 +0900

    aa

--- app/Http/Service/getRichMenu.php
+++ app/Http/Service/getRichMenu.php
@@ -80,7 +80,7 @@ $res=$this->client->rtRichMenu([
         'height'=>1686
     ],
     'selected'=> true,
-    'name'=> 'alias menuA',
+    'name'=> 'demo_1_a',
     'chatBarText'=> 'alias menu',
     //ここでarray()を使用しないと配列になってくれない。JSONで[]なってるところ。
     'areas'=> [[
@@ -119,7 +119,7 @@ $res=$this->client->rtRichMenu([
         'action'=> [
             'type'=> 'richmenuswitch',
            // 切り替え先設定
-            'richMenuAliasId'=> 'alias_dev2',
+            'richMenuAliasId'=> 'demo_1_a',
             'data'=> 'richmenu-changed-to-b'
         ]
         ]
