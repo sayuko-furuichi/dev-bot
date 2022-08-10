0bcef07b2561c53ea0219850eca866e99aa3b80f
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-10 16:49:15 +0900

    sss

--- app/Http/Service/getAnalysisData.php
+++ app/Http/Service/getAnalysisData.php
@@ -79,11 +79,10 @@ private $event;
               [
                 'type' => 'text',
                 'text' => 'タップしたURLに関する情報' . 
-                ",\n メッセージの送信数　：　". $ms[''] .
                 ",\n 　吹き出しが表示された回数：　". $ov['impression'] .
-                ",\n 　吹き出し内の動画または音声を再生開始し、75%再生した人数：　". $ov['uniqueMediaPlayed75Percent'] .
-                ",\n 　メッセージ内のいずれかのURLをタップした人数：　". $ov['mediaPlayed50Percent'] .
-                ",\n 　吹き出し内の動画または音声が再生開始された回数：　". $ov['mediaPlayed'] 
+                ",\n 　吹き出し内の動画または音声を再生開始し、75%再生した人数：　". $ms['uniqueMediaPlayed75Percent'] .
+                ",\n 　メッセージ内のいずれかのURLをタップした人数：　". $ms['mediaPlayed50Percent'] .
+                ",\n 　吹き出し内の動画または音声が再生開始された回数：　". $ms['mediaPlayed'] 
 
 
               ]
