fc897706557292855c66aac12fc3e0143a89d7bb
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-10 12:12:52 +0900

    a

--- app/Http/Controllers/LINEBotTiny.php
+++ app/Http/Controllers/LINEBotTiny.php
@@ -388,11 +388,34 @@ class LINEBotTiny
     if (strpos($http_response_header[0], '200') === false) {
         $response='request failed';
     }
+
+   $hds= parseHeaders($http_response_header);
+  $response= $hds['X-Line-Request-Id'];
+
+  
         return $response;
 
         
 
  
+}
+
+function parseHeaders( $headers )
+{
+    $head = array();
+    foreach( $headers as $k=>$v )
+    {
+        $t = explode( ':', $v, 2 );
+        if( isset( $t[1] ) )
+            $head[ trim($t[0]) ] = trim( $t[1] );
+        else
+        {
+            $head[] = $v;
+            if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
+                $head['reponse_code'] = intval($out[1]);
+        }
+    }
+    return $head;
 }
 
 
