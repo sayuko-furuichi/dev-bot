d34981906f3a8997b5639f3f467057587c2be19e
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-10 12:02:20 +0900

    あああああ

--- app/Http/Controllers/LINEBotTiny.php
+++ app/Http/Controllers/LINEBotTiny.php
@@ -365,7 +365,7 @@ class LINEBotTiny
         return $alresponse;
     }
 
-    //ブロードキャスト
+    //ブロードキャスト responceヘッダーの
 
     public function sendBroad($param){
         //
@@ -388,11 +388,6 @@ class LINEBotTiny
     if (strpos($http_response_header[0], '200') === false) {
         $response='request failed';
     }
-    $headers = getallheaders();
- if (isset($headers['X-Line-Request-Id']) || isset($headers['X-LINE-REQUEST-ID'])) {
-     $response='Life on mars?';
-
- }
         return $response;
 
         
