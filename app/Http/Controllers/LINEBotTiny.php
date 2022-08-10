6a133bbdfb02714bcdfbd7be85a118e186e61e08
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-10 11:45:19 +0900

    tameis

--- app/Http/Controllers/LINEBotTiny.php
+++ app/Http/Controllers/LINEBotTiny.php
@@ -395,12 +395,12 @@ class LINEBotTiny
 //    if(isset($headers['X-Line-Request-Id']) || isset($headers['X-LINE-REQUEST-ID']) ){
 //     $response='Life on mars?';
 //    }
-if (isset($response['X-Line-Request-Id']) || isset($response['X-LINE-REQUEST-ID'])) {
-        //     $response='Life on mars?';
-        //    }
+// if (isset($response['X-Line-Request-Id']) || isset($response['X-LINE-REQUEST-ID'])) {
+//         //     $response='Life on mars?';
+//         //    }
 
-    $response = 'life on mars?';
-}
+//     $response = 'life on mars?';
+// }
         return $response;
  
 }
