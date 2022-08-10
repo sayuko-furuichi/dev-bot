312ae6e9e23dc4dc3cc7f8fc2285dc6aa4a49ec4
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-10 10:48:07 +0900

    ss

--- app/Http/Controllers/SendMessage.php
+++ app/Http/Controllers/SendMessage.php
@@ -105,20 +105,20 @@ class SendMessage extends Controller
                     $param = new sentNarrow($channelAccessToken, $channelSecret, $client);
                     $res = $param->sendMessage();
 
-                    $client->replyMessage([
-                        'replyToken' => $event['replyToken'],
-                        'messages' => [
-                            [
-                'type' => 'text',
-                'text' =>$storeId . '　OK!'
-                            ],
+                //     $client->replyMessage([
+                //         'replyToken' => $event['replyToken'],
+                //         'messages' => [
+                //             [
+                // 'type' => 'text',
+                // 'text' =>$storeId . '　OK!'
+                //             ],
                 
-                            [
-                'type' => 'text',
-                'text' => 'OK'. $res
-                            ]
-                        ]
-                    ]);
+                //             [
+                // 'type' => 'text',
+                // 'text' => 'OK'. $res
+                //             ]
+                //         ]
+                //     ]);
                   //  $param =new getOrgMenuParam();
                    // $sId =$storeId;
                    // $param ->getParam($sId, $client, $event);
