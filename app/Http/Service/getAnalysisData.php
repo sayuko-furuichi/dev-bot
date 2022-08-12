2c4af33c3a17d672d30ce288829a51289414767f
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-12 11:16:49 +0900

    ss

--- app/Http/Service/getAnalysisData.php
+++ app/Http/Service/getAnalysisData.php
@@ -38,7 +38,7 @@ private $event;
               ],
               [
                   'type' => 'text',
-                  'text' => 'NotFound ID:' . $rqMsgId
+                  'text' => 'NotFound ID:'
               ]
           ]
               ]);
