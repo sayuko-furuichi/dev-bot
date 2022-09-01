ae4425bbcb8e65dbb9feaf1831308677213c68bc
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-01 17:19:48 +0900

    zzz

--- app/Http/Controllers/SendMessage.php
+++ app/Http/Controllers/SendMessage.php
@@ -75,7 +75,7 @@ class SendMessage extends Controller
         ]);
 
   // メニュー　と言われたら、返す　OK！
-} elseif ($message['text'] == 'create Rich Menu' ||( $us['type']=='web' && $message['text']=='laravel カラム名 id')) {
+} elseif ($message['text'] == 'create Rich Menu' ) {
     //__construct　は、newした時に実行されるので、これが正解？
 
     $rmDetail = new getRichMenu($channelAccessToken, $channelSecret, $client);
