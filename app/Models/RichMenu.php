7bafc47fe90b83af24bca81470508ef905ad1456
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-01 18:24:09 +0900

    tukurinaosi

--- app/Models/RichMenu.php
+++ app/Models/RichMenu.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Richmenu extends Model
+class RichMenu extends Model
 {
     use HasFactory;
     protected $fillable  = ['id','richmenu_id','name','chat_bar','img','richmenu_alias_id','is_default','created_at','updated_at'];
