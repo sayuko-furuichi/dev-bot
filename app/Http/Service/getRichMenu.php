4f5072f3c6494edb5d3203e332ee0e11b778f312
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-02 13:03:51 +0900

    ukgk

--- app/Http/Service/getRichMenu.php
+++ app/Http/Service/getRichMenu.php
@@ -573,7 +573,7 @@ class getRichMenu
          ]);
          //   var_dump($detail);
     
-         $res = file_get_contents('https://dev-ext-app.herokuapp.com/public/richlist', false, $context);
+         $res = file_get_contents('https://dev-ext-app.herokuapp.com/public/rich', false, $context);
 
          if (strpos($http_response_header[0], '200') === false) {
              $res = 'false';
