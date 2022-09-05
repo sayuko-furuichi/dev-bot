3bc5321515a153d5a6f80ad268dcd2001d225ae4
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-05 16:40:14 +0900

    a

--- app/Http/Service/getMember.php
+++ app/Http/Service/getMember.php
@@ -80,7 +80,7 @@ class getMember
        
         }else{
            $res= $this->client->userProf($uid);
-            $res=json_decode($res,true);
+        //    $res=json_decode($res,true);
 
             $this->client->replyMessage([
                 'replyToken' => $event['replyToken'],
