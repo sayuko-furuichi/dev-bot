d02c9d8c4b023ec8f36b76d9864c52eec25b8416
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-07 11:49:26 +0900

    richmenu_idで受け渡し

--- app/Http/Controllers/SendMessage.php
+++ app/Http/Controllers/SendMessage.php
@@ -171,8 +171,9 @@ class SendMessage extends Controller
     
 //TODO:idで受け渡しする
 } elseif ($us['type']=='web' && $message['text']=='change_df_rich_menu' && isset($message['text2']) ) {
-    $new = RichMenu::where('id',$message['text2'])->where('store_id',$storeId)->get();
+ //   $new = RichMenu::where('id',$message['text2'])->where('store_id',$storeId)->get();
 
+ $new = RichMenu::where('richmenu_id',$message['text2'])->where('store_id',$storeId)->get();
    $res= $client->defaultRm($new->richmenu_id);
    $old = RichMenu::where('is_default',1)->where('store_id',$storeId)->first();
    if(isset($old)){
