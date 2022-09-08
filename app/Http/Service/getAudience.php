a29d758e5b50c667a5bb46bf8d3484cae9ffe289
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-08 16:01:34 +0900

    create: Audience service

---
+++ app/Http/Service/getAudience.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Service;
+
+use LINE\LINEBot;
+use App\Http\Controllers\LINEBotTiny;
+use Illuminate\Http\Request;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\HTTPClient;
+use App\Models\UserProf;
+use Illuminate\Support\Facades\DB;
+
+class getAudience
+{
+    //chanell_access_token
+    private $channelAccessToken;
+    //chanell_secret
+    private $channelSecret;
+
+    //LINEBotTiny client
+    private $client;
+
+    /**
+     * Undocumented __construct
+     *
+     * @param String $channelAccessToken
+     * @param String $channelSecret
+     * @param LINEBotTiny $client
+     */
+    public function __construct(String $channelAccessToken, String $channelSecret, $client)
+    {
+        // $this->userId= $userId;
+        $this->channelAccessToken= $channelAccessToken;
+        $this->channelSecret= $channelSecret;
+        $this->client=$client;
+    }
+
+    public function createAud(){
+
+        //
+
+    }
+
+
+
+}
+
+
+
