ce5366d69fac50d6aa17aac9c877820560fe047c
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-09 09:54:28 +0900

    こうだわ

--- app/Http/Service/getAudience.php
+++ app/Http/Service/getAudience.php
@@ -53,11 +53,9 @@ class getAudience
          if($res!='request failed'){
             $ress = json_decode($res,true);
           $newaud = new Audience();
-          $newaud ->group_id=$ress->audienceGroupId;
-          $resp='ok!';
-          return $resp;
-          $newaud ->create_route=$ress->createRoute;
-          $newaud ->description=$ress->description;
+          $newaud ->group_id=$ress['audienceGroupId'];
+          $newaud ->create_route=$ress['createRoute'];
+          $newaud ->description=$ress['description'];
           $newaud->store_id=$storeId;
           $newaud->save();
         
