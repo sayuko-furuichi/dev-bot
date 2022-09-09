5988c2b521b4ebb0af523980b1c0f9895417998b
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-09 10:26:17 +0900

    s

--- database/migrations/2022_09_09_092207_create_audiences_table.php
+++ database/migrations/2022_09_09_092207_create_audiences_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('create_route');
             $table->string('description');
             $table->string('store_id');
-            $table->timestamp('expire')->nullable(true)->change();
+            $table->timestamp('expire')->nullable(true);
             $table->timestamps();
         });
     }
