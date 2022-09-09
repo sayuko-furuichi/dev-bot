22ca555a6855d24e738fa0e508f6b29bc62579eb
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-09 09:32:03 +0900

    s

--- app/Http/Service/getAudience.php
+++ app/Http/Service/getAudience.php
@@ -50,17 +50,17 @@ class getAudience
             //  ] 
         ]);
 
-        //  if($res!='request failed'){
-        //     $ress = json_decode($res,true);
-        //   $newaud = new Audience;
-        //   $newaud ->group_id=$ress->audienceGroupId;
-        //   $newaud ->create_route=$ress->createRoute;
-        //   $newaud ->description=$ress->description;
-        //   $newaud->store_id=$storeId;
-        //   $newaud->save();
-        //   $resp='ok!';
-        //  }
-        return $res;
+         if($res!='request failed'){
+            $ress = json_decode($res,true);
+          $newaud = new Audience;
+          $newaud ->group_id=$ress->audienceGroupId;
+          $newaud ->create_route=$ress->createRoute;
+          $newaud ->description=$ress->description;
+          $newaud->store_id=$storeId;
+          $newaud->save();
+          $resp='ok!';
+         }
+        return $resp;
         //
 
     }
