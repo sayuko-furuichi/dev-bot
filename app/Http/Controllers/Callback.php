3b320d433673b01a4f7aa5fafa1ba9019f9c9507
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-16 17:05:09 +0900

    store_idをいくらでも受け付けられるように

--- app/Http/Controllers/Callback.php
+++ app/Http/Controllers/Callback.php
@@ -13,31 +13,39 @@ use LINE\LINEBot\MessageBuilder;
 
 use App\Http\Controllers\SendMessage;
 use App\Http\Service\getUserProf;
+use App\Models\Store;
 
 class Callback extends Controller
 {
     public function callback(Request $request)
     {
-        //本店
-        if ($request->store_id == '1') {
-            $channelAccessToken = '/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=';
-            $channelSecret = '0b0aadd7b81ec25d7d861c28846e4048';
-            $storeId=$request->store_id;
+    //     //本店
+    //     if ($request->store_id == '1') {
+    //         $channelAccessToken = '/4Ejv8i8d4NB1+KSUMMXZA7zEGoCpcBQgIbEng9HYYgcOL1xPcgolcwDSXKbOlRxHvUUhmocgJDvFQrqH7IfpMkxYBt1O2NcU0wSU8bPIIDI9Rpe2VQCHa7ngQp57ptBA7oEAkNxdkZTweVAR0RF1QdB04t89/1O/w1cDnyilFU=';
+    //         $channelSecret = '0b0aadd7b81ec25d7d861c28846e4048';
+    //         $storeId=$request->store_id;
          
 
-       //2号店
-        } elseif ($request->store_id == '2') {
-            $channelAccessToken ='8jCwB7uJHNCjdiXhcUmlpFheWXIAUB2mnScBaSBvcSPp209NiJ9c/dTdKv3EF0+ufDJFR7ZBZ3MhcRe7RfFO6iGo5DugZXsO+Hdw7hht2JfYP/m/HgOvWl5FDkrrAKKZUddiWqaBY5rSytRV7q/POQdB04t89/1O/w1cDnyilFU=';
-            $channelSecret ='b1361c02edc2e4fe84b7bef6c3bddf9d';
-            $storeId=$request->store_id;
+    //    //2号店
+    //     } elseif ($request->store_id == '2') {
+    //         $channelAccessToken ='8jCwB7uJHNCjdiXhcUmlpFheWXIAUB2mnScBaSBvcSPp209NiJ9c/dTdKv3EF0+ufDJFR7ZBZ3MhcRe7RfFO6iGo5DugZXsO+Hdw7hht2JfYP/m/HgOvWl5FDkrrAKKZUddiWqaBY5rSytRV7q/POQdB04t89/1O/w1cDnyilFU=';
+    //         $channelSecret ='b1361c02edc2e4fe84b7bef6c3bddf9d';
+    //         $storeId=$request->store_id;
 
-        //3号店
-        } elseif ($request->store_id == '3') {
-            $channelAccessToken ='paq5m/NvcbF96Rk5UUJoQlbLAS7FD5LRYMqzEWM+Ov8q1mb87WZWSd53enK+feHdV+pHfh6o3in2KI/cY4tEWmCJ7VOCuBHBSNkveeGFULhHWdLswizYiafmhee9yRgATphoOA1ZUY5F53R6/S4VigdB04t89/1O/w1cDnyilFU=';
-            $channelSecret ='0bedd5514772fb1d4160813f870386d4';
-            $storeId=$request->store_id;
-        }
+    //     //3号店
+    //     } elseif ($request->store_id == '3') {
+    //         $channelAccessToken ='paq5m/NvcbF96Rk5UUJoQlbLAS7FD5LRYMqzEWM+Ov8q1mb87WZWSd53enK+feHdV+pHfh6o3in2KI/cY4tEWmCJ7VOCuBHBSNkveeGFULhHWdLswizYiafmhee9yRgATphoOA1ZUY5F53R6/S4VigdB04t89/1O/w1cDnyilFU=';
+    //         $channelSecret ='0bedd5514772fb1d4160813f870386d4';
+    //         $storeId=$request->store_id;
+    //     }
 
+       $store =Store::where('id',$request->store_id)->first();
+       if(isset($store)){
+
+       $storeId=$request->store_id;
+       $channelAccessToken=$store->cat;
+       $channelSecret =$store->cs;
+    }
            //DB共有できたら生き返らせる
         // $pr = UserProf::where('id',$request->store_id)->first();
             
