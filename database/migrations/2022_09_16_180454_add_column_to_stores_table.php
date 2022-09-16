df8743bc5727831acc905f8f790c408469169fa3
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-16 18:06:12 +0900

    a

---
+++ database/migrations/2022_09_16_180454_add_column_to_stores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stores', function (Blueprint $table) {
+            //
+            $table->string('card_url');
+            $table->string('coupon_url');
+            $table->string('liff_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stores', function (Blueprint $table) {
+            //
+        });
+    }
+};
