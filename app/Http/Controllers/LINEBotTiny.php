0a61de79d434c1e82ee4bc398c236af76a494e86
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-20 17:37:30 +0900

    err 確認

--- app/Http/Controllers/LINEBotTiny.php
+++ app/Http/Controllers/LINEBotTiny.php
@@ -171,7 +171,7 @@ class LINEBotTiny
 
         $rmresponse = file_get_contents('https://api.line.me/v2/bot/richmenu', false, $rmcontext);
         if (strpos($http_response_header[0], '200') === false) {
-            $rmresponse = 'false';
+          //  $rmresponse = 'false';
         }
         return $rmresponse;
     }
