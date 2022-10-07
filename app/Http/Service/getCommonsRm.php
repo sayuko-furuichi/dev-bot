b93ca9d90bfb7e986f7683af9be7475ffc4c04eb
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-07 16:16:31 +0900

    s

--- app/Http/Service/getCommonsRm.php
+++ app/Http/Service/getCommonsRm.php
@@ -68,12 +68,12 @@ class getCommonsRm
       
         //create rich menu A
         $res= $this->createRmA($rmA,$simg);
-        $res= json_decode($res, true);
+        $rs= json_decode($res, true);
         $rmA->richmenu_id=$rs['richMenuId'];
    
         //create rich menu B
          $res= $this->createRmB($rmB,$simg);
-         $res= json_decode($res, true);
+         $rs= json_decode($res, true);
          $rmB->richmenu_id=$rs['richMenuId'];
 
     
