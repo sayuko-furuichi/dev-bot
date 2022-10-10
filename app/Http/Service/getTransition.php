b3d418a30060ccbf384fe839d28a371f786c0e4f
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-10 14:46:38 +0900

    s

--- app/Http/Service/getTransition.php
+++ app/Http/Service/getTransition.php
@@ -40,7 +40,7 @@ class getTransition
      function insertData($userId,$data,$event){
         $oldTrans=$this->search($userId);
 
-        if(empty($oldTrans)){
+        if(empty($oldTrans)==true){
           
             $trans =new Transition;
             $trans->lineuser_id=$userId;
