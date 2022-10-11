c68f318121e20473870b4003e5ef20ac18dca0f7
sayuko-furuichi <zack.dog009@gmail.com>
2022-10-11 16:19:59 +0900

    あ

--- app/Http/Controllers/CatchEvents.php
+++ app/Http/Controllers/CatchEvents.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\LINEBotTiny;
 
-use LINE\LINEBot\Constant\HTTPHeader;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot;
 use Illuminate\Support\Facades\Storage;
 
